b396d76a18196ccfa4598a3face9730f170a3ac5
Rodolfo <rodolfocastillomateluna@gmail.com>
2018-09-07 12:07:39 -0300

    Improve docker scripts

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker run --rm -t -v `pwd`:/volume -v cargo_cache:/root/.cargo clux/muslrust cargo build --release
+
 mkdir -p ./tmp_bin
 cp ./target/x86_64-unknown-linux-musl/release/directemar_crawler ./tmp_bin
 
